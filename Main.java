71a5c49aa6b1811666559064b7a5304c8b376602
ACzerw3 <126687767+ACzerw3@users.noreply.github.com>
2023-05-24 12:49:32 +0000

    Commit 2

--- Main.java
+++ Main.java
@@ -4,18 +4,21 @@ class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int x;
-    System.out.println("Podaj rozmiar tablicy: ");
+    System.out.print("Podaj rozmiar tablicy: ");
     x = scanner.nextInt();
+    System.out.println(" ");
     int tablica1[] = new int[x];
     for(int i=0; i<x; i++)
       {
-      System.out.println("Podaj element tablicy: ");
+      System.out.print("Podaj element tablicy: ");
       tablica1[i] = scanner.nextInt();
       }
-
+      System.out.println(" ");
+    
     for(int j=0; j<x; j++)
       System.out.println("Kolejny element pierwszej tablicy to: " +tablica1[j]);
-
+      System.out.println(" ");
+    
     int tablica2[] = new int[x];
     for(int k=0; k<x; k++)
       {
